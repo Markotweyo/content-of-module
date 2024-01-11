33af6cfb4fca5396c1ac5a2c6a629f0d02fa38ee
Markotweyo <markotweyo@gmail.com>
2024-01-11 13:22:55 +0300

    loop through string and filter with break statement

--- loops.py
+++ loops.py
@@ -1,15 +1,27 @@
-
-def printFruits():
+##Looping though arrays
+def print_fruits():
     fruits=['apple', 'orange', 'banana', 'mango']
     for fruit in fruits:
         if fruit=='banana':
             break
         print(fruit)
     
-printFruits()
+print_fruits()
+
+
+
+#Looping through the letters in a string
+
+def looping_string():
+    for x in "banana":
+     print(x)
+
+looping_string()
+
+
 
 #def main():
-#    printFruits()
+#   looping_string()
     
 #if __name__ == '__main__':
 #    main()
